e3bf52942511b98614892c2b2ead87e9c05979c3
halinh0605 <linhha0605@gmail.com>
2019-05-06 21:04:18 +0700

    a

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,108 @@
+@extends('master')
+@section('content')
+<section class="section_gap">
+    <!-- single product slide -->
+    <div class="single-product-slider">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 text-center">
+                    <div class="section-title">
+                        <h1>Sản phẩm bán chạy</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore
+                            magna aliqua.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <!-- single product -->
+                @foreach($product as $item)
+                    <div class="col-lg-3 col-md-6">
+                        <div class="single-product">
+                            <img class="img-fluid" src="{!! $item->hinhanh !!}" alt="">
+                            <div class="product-details">
+                                <h6>{!! $item->tensp !!}</h6>
+                                <div class="price">
+                                    <h6>{!! number_format($item->gia,0,",",".") !!} VNĐ</h6>
+                                    {{--<h6 class="l-through">$210.00</h6>--}}
+                                </div>
+                                <div class="prd-bottom">
+
+                                    <a href="" class="social-info">
+                                        <span class="ti-bag"></span>
+                                        <p class="hover-text">add to bag</p>
+                                    </a>
+                                    <a href="" class="social-info">
+                                        <span class="lnr lnr-heart"></span>
+                                        <p class="hover-text">Wishlist</p>
+                                    </a>
+                                    <a href="" class="social-info">
+                                        <span class="lnr lnr-sync"></span>
+                                        <p class="hover-text">compare</p>
+                                    </a>
+                                    <a href="" class="social-info">
+                                        <span class="lnr lnr-move"></span>
+                                        <p class="hover-text">view more</p>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+
+            </div>
+        </div>
+    </div>
+    <!-- single product slide -->
+    <div class="single-product-slider">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 text-center">
+                    <div class="section-title">
+                        <h1>Sản phẩm mới</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore
+                            magna aliqua.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <!-- single product -->
+                @foreach($product as $item)
+                    <div class="col-lg-3 col-md-6">
+                        <div class="single-product">
+                            <img class="img-fluid" src="{!! $item->hinhanh !!}" alt="">
+                            <div class="product-details">
+                                <h6>{!! $item->tensp !!}</h6>
+                                <div class="price">
+                                    <h6>{!! number_format($item->gia,0,",",".") !!} VNĐ</h6>
+                                    {{--<h6 class="l-through">$210.00</h6>--}}
+                                </div>
+                                <div class="prd-bottom">
+
+                                    <a href="" class="social-info">
+                                        <span class="ti-bag"></span>
+                                        <p class="hover-text">add to bag</p>
+                                    </a>
+                                    <a href="" class="social-info">
+                                        <span class="lnr lnr-heart"></span>
+                                        <p class="hover-text">Wishlist</p>
+                                    </a>
+                                    <a href="" class="social-info">
+                                        <span class="lnr lnr-sync"></span>
+                                        <p class="hover-text">compare</p>
+                                    </a>
+                                    <a href="" class="social-info">
+                                        <span class="lnr lnr-move"></span>
+                                        <p class="hover-text">view more</p>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
